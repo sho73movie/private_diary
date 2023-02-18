c28c387a6b1a3595d190da82f3cf01b3a8cb0b37
sho73movie <sn112867@gmail.com>
2023-02-18 15:03:51 +0900

    トップページ、問い合わせページの作成

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# statiicディレクトリの絶対パスを設定
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
